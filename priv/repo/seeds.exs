1a6c75e77ced84a9ccf25b87eeeef9b9d7fadc2d
Dingchang <i.septentrion@gmail.com>
2017-10-12 03:43:31 -0400

    Add bootstrap

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,14 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Microblog.Repo
+alias Microblog.Accounts.User
+
+Repo.delete_all(User)
+
+Repo.insert!(%User{email: "abe@example.com", username: "Abe"})
+Repo.insert!(%User{email: "bill@example.com", username: "Bill"})
+Repo.insert!(%User{email: "carlo@example.com", username: "Carlo"})
+Repo.insert!(%User{email: "jack@example.com", username: "Jack"})
+Repo.insert!(%User{email: "kim@example.com", username: "Kim"})
